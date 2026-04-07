936a95a6b0a4139ef112d0f06862f450b6302ff0
Chang Yu <marcus.yu.56@gmail.com>
2026-04-07 01:17:04 -0700

    Clean up while loop

--- lexer.c
+++ lexer.c
@@ -14,13 +14,13 @@ static char* read_file(char* p) {
     goto cleanup;
   }
  
-  int read_res = 1;
-  while(read_res) {
+  while(1) {
     char buf[4096];
-    read_res = fread(buf,  1, sizeof(buf), f);
-    if (read_res != 0) {
-      fwrite(buf, 1, read_res, out_stream);
+    int res = fread(buf,  1, sizeof(buf), f);
+    if (res == 0) {
+      break;
     }
+    fwrite(buf, 1, res, out_stream);
   }
   fflush(out_stream);
 
