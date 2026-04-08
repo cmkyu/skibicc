fb77193f03e34c9771df4e5527a55dbeebf21a1f
Chang Yu <marcus.yu.56@gmail.com>
2026-04-07 21:28:31 -0700

    Add basic command line arg parsing.

--- lexer.c
+++ lexer.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include "status.h"
 
-static char* read_file(char* p) {
-  char* out_buf = NULL;
+char* read_file(char* p) {
+  FILE* f = fopen(p, "r");
+  if (!f) {
+    fprintf(stderr, "readfile(): failed to open file: %s.\n", p);
+    return NULL;
+  }
+ 
+  char* out_buf;
   size_t out_size;
   FILE* out_stream = open_memstream(&out_buf, &out_size); 
   if (!out_stream) {
-    goto cleanup;
+    fprintf(stderr, "readfile(): failed to create output stream.\n");
+    fclose(f);
+    return NULL;
   }
 
-  FILE* f = fopen(p, "r");
-  if (!f) {
-    goto cleanup;
-  }
- 
   while(1) {
     char buf[4096];
     int res = fread(buf,  1, sizeof(buf), f);
@@ -23,15 +26,8 @@ static char* read_file(char* p) {
     fwrite(buf, 1, res, out_stream);
   }
   fflush(out_stream);
-
-cleanup:
   fclose(out_stream);
   fclose(f);
   return out_buf;
 }
 
-int main(int argc, char* argv[]) {
-  char* res = read_file("test.txt");
-  printf("Got file: %s", res);
-  return 0;
-}
--- skibicc.c
+++ skibicc.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 #include "lexer.h"
+#include <getopt.h>
+
+static struct option opts[] = {
+  {"lex", no_argument, NULL, 0 },
+  {"parse", no_argument, NULL, 1 },
+  {"codegen", no_argument, NULL, 2 },
+};
+
+typedef enum compiler_option {
+  LEX,
+  PARSE,
+  CODEGEN,
+  DEFAULT,
+} compiler_option;
 
 int main(int argc, char* argv[]) {
-  char* res = read_file("test.txt");
+  int c;
+  int opt_index;
+  compiler_option opt = DEFAULT;
+  while (1) {
+    c = getopt_long_only(argc, argv, "", opts, &opt_index);
+    if (c == -1) {
+      break;
+    }
+    
+    switch (c) {
+      case 0:
+        opt = LEX;
+        break;
+      case 1:
+        opt = PARSE;
+        break;
+      case 2:
+        opt = CODEGEN;
+        break;
+      default:
+        fprintf(stderr, "Unknown argument code: %c.\n", c);
+        break;
+    }
+  }
+
+  printf("Got argument: %s.\n", opts[opt_index].name);
+
+  if (optind >= argc) {
+    fprintf(stderr, "Input file path not specified\n");
+    return 1;
+  }
+
+  char* path = argv[optind];
+  printf("Got input file path: %s\n", path);
+  char* res = read_file(path);
   printf("Got file: %s", res);
   return 0;
 }
