9dec3372624b4c927dc2a3fae1b8bcac4bf7b8ae
Chang Yu <marcus.yu.56@gmail.com>
2026-04-07 23:52:53 -0700

    Format lexer.c

--- lexer.c
+++ lexer.c
@@ -1,16 +1,17 @@
-#include "status.h"
 #include <stdio.h>
 
-char *read_file(char *p) {
-  FILE *f = fopen(p, "r");
+#include "status.h"
+
+char* read_file(char* p) {
+  FILE* f = fopen(p, "r");
   if (!f) {
     fprintf(stderr, "readfile(): failed to open file: %s.\n", p);
     return NULL;
   }
 
-  char *out_buf;
+  char* out_buf;
   size_t out_size;
-  FILE *out_stream = open_memstream(&out_buf, &out_size);
+  FILE* out_stream = open_memstream(&out_buf, &out_size);
   if (!out_stream) {
     fprintf(stderr, "readfile(): failed to create output stream.\n");
     fclose(f);
