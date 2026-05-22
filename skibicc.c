aae1278e7a3f32b7cbcb1c4d5aa726d6fae031f5
Chang Yu <marcus.yu.56@gmail.com>
2026-05-21 23:01:48 -0700

    Modify main so that we can run it end to end.

--- skibicc.c
+++ skibicc.c
@@ -5,8 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "errors.h"
+#include "array.h"
+#include "codegen.h"
+#include "ir.h"
 #include "lexer.h"
+#include "parser.h"
 #include "strings.h"
 
 static struct option opts[] = {
@@ -24,7 +27,7 @@ typedef enum compiler_option {
 } compiler_option;
 
 //! Runs GNU C preprocessor for file specified by `path`. `path` is replaced
-//! with the path to the output file after execution finishes.
+//! with the path to the output file (.i file) after execution finishes.
 static int run_preprocessor(char* path) {
   char* path_copy = strdup(path);
   replace_ext(&path, "i");
@@ -37,12 +40,33 @@ static int run_preprocessor(char* path) {
   return res;
 }
 
-//! Removes the file `path`.
-static int rm_file(const char* path) {
-  // command: rm path
-  char* command = string_concat(2, "rm ", path);
+//! Runs GNU assembler for file specified by `path`.
+static int emit_code(char* path) {
+  char* obj_path = strdup(path);
+  replace_ext(&obj_path, "o");
+  // command: as -o obj_path -c path
+  char* command = string_concat(4, "as --64 -o ", obj_path, " -c ", path);
   int res = system(command);
   free(command);
+  if (res != 0) {
+    free(obj_path);
+    return res;
+  }
+
+  char* prog_path = strdup(path);
+  replace_ext(&prog_path, "out");
+  // TODO: this is hard-coded. Write a function to find lib paths.
+  command = string_concat(
+      5, "ld -o ", prog_path,
+      " -m elf_x86_64 /usr/lib/Scrt1.o /usr/lib/crti.o "
+      "/lib/gcc/x86_64-pc-linux-gnu/16.1.1/crtbeginS.o "
+      "-dynamic-linker /lib64/ld-linux-x86-64.so.2 ",
+      obj_path,
+      " -lc /lib/gcc/x86_64-pc-linux-gnu/16.1.1/crtendS.o /usr/lib/crtn.o");
+  res = system(command);
+  free(command);
+  free(prog_path);
+  free(obj_path);
   return res;
 }
 
@@ -75,18 +99,31 @@ int main(int argc, char* argv[]) {
     fprintf(stderr, "Input file path not specified\n");
     return 1;
   }
-  printf("Got argument: %s.\n",
-         opt == CO_DEFAULT ? "<none>" : opts[opt_index].name);
 
   char* path = strdup(argv[optind]);
-  strcpy(path, argv[optind]);
-  printf("Got input file path: %s\n", path);
+  if (run_preprocessor(path) != 0) {
+    return 1;
+  }
+  char* text = read_file(path);
+  remove(path);
 
-  run_preprocessor(path);
-  char* res = read_file(path);
-  printf("Got file: %s\n", res);
-  rm_file(path);
+  array tokens = lex(text);
+  if (opt == CO_LEX) {
+    goto end;
+  }
+  ast_node* ast = parse(&tokens);
+  if (opt == CO_PARSE) {
+    goto end;
+  }
 
+  replace_ext(&path, "s");
+  FILE* f = fopen(path, "w+");
+  emit(f, emit_ir(ast));
+  fclose(f);
+  emit_code(path);
+  // TODO: implement memory clean up for tokens, AST, IR and ASM.
+end:
   free(path);
+  free(text);
   return 0;
 }
