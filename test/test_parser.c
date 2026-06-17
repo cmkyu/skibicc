a82b0bbabde8a8b2f8a5d4942efa6196bf10f53a
Chang Yu <marcus.yu.56@gmail.com>
2026-06-16 20:30:53 -0700

    Parser unit test for bit operators.

--- test/test_parser.c
+++ test/test_parser.c
@@ -12,6 +12,7 @@ void test_parser_basic(void) {
   char text[] = "int main(void) { return 2; }";
   array tokens = lex(text, "test.c");
   ast_node* ast = parse(&tokens);
+  prettyprint_ast(ast);
   ast_destroy(ast);
   destroy_tokens(&tokens);
 }
@@ -119,10 +120,63 @@ void test_parser_binary_expression(void) {
   destroy_tokens(&tokens);
 }
 
+void test_parser_bit_operators(void) {
+  char text[] = "int main(void){ return 1 << 2 >> 3 | 4 ^ 5 & 6;}";
+  array tokens = lex(text, "test.c");
+  ast_node* ast = parse(&tokens);
+  ast_node* root = ast;
+  prettyprint_ast(ast);
+
+  TEST_ASSERT_EQUAL(AST_RETSTMNT, ast->node_type);
+
+  ast = ast->node.statement->expression;
+  TEST_ASSERT_EQUAL(AST_EXPR, ast->node_type);
+  TEST_ASSERT_EQUAL(OP_BITOR, ast->node.expression->op->op_type);
+
+  ast_node* lhs = ast->node.expression->lhs;
+  TEST_ASSERT_EQUAL(AST_EXPR, lhs->node_type);
+  TEST_ASSERT_EQUAL(OP_SHR, lhs->node.expression->op->op_type);
+  ast_node* rhs = ast->node.expression->rhs;
+  TEST_ASSERT_EQUAL(AST_EXPR, rhs->node_type);
+  TEST_ASSERT_EQUAL(OP_BITXOR, rhs->node.expression->op->op_type);
+
+  {
+    ast_node* ast = lhs;
+    ast_node* lhs = ast->node.expression->lhs;
+    TEST_ASSERT_EQUAL(AST_EXPR, lhs->node_type);
+    TEST_ASSERT_EQUAL(OP_SHL, lhs->node.expression->op->op_type);
+    ast_node* rhs = ast->node.expression->rhs;
+    TEST_ASSERT_EQUAL(AST_CONST, rhs->node_type);
+
+    ast = lhs;
+    lhs = ast->node.expression->lhs;
+    TEST_ASSERT_EQUAL(AST_CONST, lhs->node_type);
+    rhs = ast->node.expression->rhs;
+    TEST_ASSERT_EQUAL(AST_CONST, rhs->node_type);
+  }
+
+  ast = rhs;
+  lhs = ast->node.expression->lhs;
+  TEST_ASSERT_EQUAL(AST_CONST, lhs->node_type);
+  rhs = ast->node.expression->rhs;
+  TEST_ASSERT_EQUAL(AST_EXPR, rhs->node_type);
+  TEST_ASSERT_EQUAL(OP_BITAND, rhs->node.expression->op->op_type);
+
+  ast = rhs;
+  lhs = ast->node.expression->lhs;
+  TEST_ASSERT_EQUAL(AST_CONST, lhs->node_type);
+  rhs = ast->node.expression->rhs;
+  TEST_ASSERT_EQUAL(AST_CONST, rhs->node_type);
+
+  ast_destroy(root);
+  destroy_tokens(&tokens);
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_parser_basic);
   RUN_TEST(test_parser_unary_expression);
   RUN_TEST(test_parser_binary_expression);
+  RUN_TEST(test_parser_bit_operators);
   return UNITY_END();
 }
