6625a1b36014deac201335842440fc1aeb985593
Chang Yu <marcus.yu.56@gmail.com>
2026-06-22 19:48:49 -0700

    Unit test for relational ops parsing.

--- test/test_parser.c
+++ test/test_parser.c
@@ -172,11 +172,64 @@ void test_parser_bit_operators(void) {
   destroy_tokens(&tokens);
 }
 
+void test_parser_relational_and_logical_operators(void) {
+  char text[] = "int main(void){ return 1 < 2 >= 3 || 4 && 5 != 6;}";
+  array tokens = lex(text, "test.c");
+  ast_node* ast = parse(&tokens);
+  ast_node* root = ast;
+  prettyprint_ast(ast);
+
+  TEST_ASSERT_EQUAL(AST_RETSTMNT, ast->node_type);
+
+  ast = ast->node.statement->expression;
+  TEST_ASSERT_EQUAL(AST_EXPR, ast->node_type);
+  TEST_ASSERT_EQUAL(OP_OR, ast->node.expression->op->op_type);
+
+  ast_node* lhs = ast->node.expression->lhs;
+  TEST_ASSERT_EQUAL(AST_EXPR, lhs->node_type);
+  TEST_ASSERT_EQUAL(OP_GE, lhs->node.expression->op->op_type);
+  ast_node* rhs = ast->node.expression->rhs;
+  TEST_ASSERT_EQUAL(AST_EXPR, rhs->node_type);
+  TEST_ASSERT_EQUAL(OP_AND, rhs->node.expression->op->op_type);
+
+  {
+    ast_node* ast = lhs;
+    ast_node* lhs = ast->node.expression->lhs;
+    TEST_ASSERT_EQUAL(AST_EXPR, lhs->node_type);
+    TEST_ASSERT_EQUAL(OP_LT, lhs->node.expression->op->op_type);
+    ast_node* rhs = ast->node.expression->rhs;
+    TEST_ASSERT_EQUAL(AST_CONST, rhs->node_type);
+
+    ast = lhs;
+    lhs = ast->node.expression->lhs;
+    TEST_ASSERT_EQUAL(AST_CONST, lhs->node_type);
+    rhs = ast->node.expression->rhs;
+    TEST_ASSERT_EQUAL(AST_CONST, rhs->node_type);
+  }
+
+  ast = rhs;
+  lhs = ast->node.expression->lhs;
+  TEST_ASSERT_EQUAL(AST_CONST, lhs->node_type);
+  rhs = ast->node.expression->rhs;
+  TEST_ASSERT_EQUAL(AST_EXPR, rhs->node_type);
+  TEST_ASSERT_EQUAL(OP_NE, rhs->node.expression->op->op_type);
+
+  ast = rhs;
+  lhs = ast->node.expression->lhs;
+  TEST_ASSERT_EQUAL(AST_CONST, lhs->node_type);
+  rhs = ast->node.expression->rhs;
+  TEST_ASSERT_EQUAL(AST_CONST, rhs->node_type);
+
+  ast_destroy(root);
+  destroy_tokens(&tokens);
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_parser_basic);
   RUN_TEST(test_parser_unary_expression);
   RUN_TEST(test_parser_binary_expression);
   RUN_TEST(test_parser_bit_operators);
+  RUN_TEST(test_parser_relational_and_logical_operators);
   return UNITY_END();
 }
